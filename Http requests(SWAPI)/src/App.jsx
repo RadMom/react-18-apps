3f0801e5798befc37708ab41991c728c80aff2d6
RadMom <RadMom1111@gmail.com>
2023-06-05 15:13:52 +0300

    180. Using useEffect for Http requests

--- Http requests(SWAPI)/src/App.jsx
+++ Http requests(SWAPI)/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,12 +8,12 @@ function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    async function fetchMoviesHandler() {
+    const fetchMoviesHandler = useCallback(async () => {
         setIsLoading(true);
         setError(null);
 
         try {
-            const response = await fetch("https://swapi.dev/api/film/");
+            const response = await fetch("https://swapi.dev/api/films/");
             if (!response.ok) {
                 throw new Error("Something went wrong");
             }
@@ -34,7 +34,12 @@ function App() {
             setError(error.message);
         }
         setIsLoading(false);
-    }
+    }, []);
+
+    useEffect(() => {
+        //useEffect is here because it must be after fetchMoviesHandler definition
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
 
     let content = <p>Found NO movies</p>;
     if (movies.length > 0) {

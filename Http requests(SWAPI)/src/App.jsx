c30ca364e5cf94733e6469f374182b4ebbabd145
RadMom <RadMom1111@gmail.com>
2023-06-06 10:22:14 +0300

    182. POST request,Firebase

--- Http requests(SWAPI)/src/App.jsx
+++ Http requests(SWAPI)/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -13,23 +14,26 @@ function App() {
         setError(null);
 
         try {
-            const response = await fetch("https://swapi.dev/api/films/");
+            const response = await fetch(
+                "https://react-http-movies-app-bdcec-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+            );
             if (!response.ok) {
                 throw new Error("Something went wrong");
             }
 
             const data = await response.json();
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    releaseDate: movieData.release_date,
-                    openingText: movieData.opening_crawl,
-                };
-            });
+            const loadedMovies = [];
+            for (const key in data) {   
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
 
-            setMovies(transformedMovies);
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -40,6 +44,21 @@ function App() {
         //useEffect is here because it must be after fetchMoviesHandler definition
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch(
+            "https://react-http-movies-app-bdcec-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+            {
+                method: "POST",
+                body: JSON.stringify(movie), //body doen't want object. It accepts JSON data
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        const data = await response.json();
+        console.log(data);
+    }
 
     let content = <p>Found NO movies</p>;
     if (movies.length > 0) {
@@ -54,6 +73,9 @@ function App() {
 
     return (
         <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
             <section>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>

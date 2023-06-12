c9069aea6d261cdb3b8f3a46ae056cf80114b3c1
RadMom <RadMom1111@gmail.com>
2023-06-12 10:33:43 +0300

    209. FirstName,LastName,Email Form

--- Section 16- Forms/src/components/BasicForm.jsx
+++ Section 16- Forms/src/components/BasicForm.jsx
@@ -1,31 +1,98 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+    const {
+        value: enteredFisrtName,
+        isValid: firstNameIsValid,
+        hasError: firstNameIsInvalid,
+        valueChangeHandler: firstNameChangeHandler,
+        inputBlurHandler: firstNameBlurHandler,
+        reset: resetFirstName,
+    } = useInput((enteredFirstName) => enteredFirstName.trim() !== "");
+
+    const {
+        value: enteredLastName,
+        isValid: lastNameIsValid,
+        hasError: lastNameIsInvalid,
+        valueChangeHandler: lastNameChangeHandler,
+        inputBlurHandler: lastNameBlurHandler,
+        reset: resetLasttName,
+    } = useInput((enteredLastName) => enteredLastName.trim() !== "");
+
+    const {
+        value: enteredEmail,
+        isValid: emailIsValid,
+        hasError: emailIsInvalid,
+        valueChangeHandler: emailChangeHandler,
+        inputBlurHandler: emailBlurHandler,
+        reset: resetEmail,
+    } = useInput((enteredEmail) => enteredEmail.includes("@"));
+
+    let formIsValid = false;
+
+    if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+        formIsValid = true;
+    }
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault();
+
+        if (!formIsValid) {
+            return;
+        }
+        console.log(enteredFisrtName, enteredLastName, enteredEmail);
+
+        resetFirstName();
+        firstNameBlurHandler;
+        resetLasttName();
+        lastNameBlurHandler;
+        resetEmail();
+        emailBlurHandler;
+    };
+
+    const firstNameInputClasses = firstNameIsInvalid ? "form-control invalid" : "form-control";
+    const lastNameInputClasses = lastNameIsInvalid ? "form-control invalid" : "form-control";
+    const emailInputClasses = emailIsInvalid ? "form-control invalid" : "form-control";
+
     return (
-        <form>
+        <form onSubmit={formSubmissionHandler}>
             <div className="control-group">
-                <div className="form-control">
-                    <label htmlFor="name">First Name</label>
+                <div className={firstNameInputClasses}>
+                    <label htmlFor="firstName">First Name</label>
                     <input
                         type="text"
-                        id="name"
+                        id="firstName"
+                        onChange={firstNameChangeHandler}
+                        onBlur={firstNameBlurHandler}
+                        value={enteredFisrtName}
                     />
+                    {firstNameIsInvalid && <p className="error-text">Enter valid Fist Name</p>}
                 </div>
-                <div className="form-control">
-                    <label htmlFor="name">Last Name</label>
+                <div className={lastNameInputClasses}>
+                    <label htmlFor="lastName">Last Name</label>
                     <input
                         type="text"
-                        id="name"
+                        id="lastName"
+                        onChange={lastNameChangeHandler}
+                        onBlur={lastNameBlurHandler}
+                        value={enteredLastName}
                     />
+                    {lastNameIsInvalid && <p className="error-text">Enter valid Last Name</p>}
                 </div>
             </div>
-            <div className="form-control">
-                <label htmlFor="name">E-Mail Address</label>
+            <div className={emailInputClasses}>
+                <label htmlFor="email">E-Mail Address</label>
                 <input
-                    type="text"
-                    id="name"
+                    type="email"
+                    id="email"
+                    onChange={emailChangeHandler}
+                    onBlur={emailBlurHandler}
+                    value={enteredEmail}
                 />
+                {emailIsInvalid && <p className="error-text">Enter valid Email</p>}
             </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     );

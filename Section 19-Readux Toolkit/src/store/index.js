067b52f6ddc3d15603585c62bb9581e22f7053f8
RadMom <RadMom1111@gmail.com>
2023-06-14 14:52:24 +0300

    278. Auth Slice added. Can add real authentication

--- Section 19-Readux Toolkit/src/store/index.js
+++ Section 19-Readux Toolkit/src/store/index.js
@@ -1,5 +1,6 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
+//counter
 const initialCounterState = {
     counter: 0,
     showCounter: true,
@@ -16,8 +17,6 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action) {
-            console.log(state.counter);
-            console.log(action.type);
             state.counter = state.counter + action.payload;
         },
         toggleCounter(state) {
@@ -26,10 +25,32 @@ const counterSlice = createSlice({
     },
 });
 
+//authentication
+const initialAuthState = {
+    isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+    name: "authentication",
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        },
+    },
+});
+
 const store = configureStore({
-    reducer: counterSlice.reducer,
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer,
+    },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;

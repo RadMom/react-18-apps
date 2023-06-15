c904a0c0f2b944993aa2a6451520fbe13a933868
RadMom <RadMom1111@gmail.com>
2023-06-15 10:20:59 +0300

    cartSlice ready

---
+++ Section 20- Redux Toolkit Advanced/src/store/cart-slice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+    },
+    reducers: {
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+
+            //We can add only 1 item!!!
+            state.totalQuantity++;
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    name: newItem.name,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            state.totalQuantity--;
+            if (existingItem.quantity === 1) {
+                state.items.filter((item) => item.id !== id); //return an array with items with !==ids
+            } else {
+                existingItem.quantity--;
+            }
+        },
+    },
+});
+
+export const cartAcrions = cartSlice.actions;
+
+export default cartSlice;

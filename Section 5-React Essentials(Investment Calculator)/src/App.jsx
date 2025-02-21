76770d74c7db319dfb79bfecb93e8a817425233b
RadMom <RadMom1111@gmail.com>
2025-02-21 10:16:55 +0200

    Section 5 finished

--- Section 5-React Essentials(Investment Calculator)/src/App.jsx
+++ Section 5-React Essentials(Investment Calculator)/src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    const inputIsValid = userInput.duration >= 1;
+
+    function handleChangeInput(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue,
+            };
+        });
+    }
+    return (
+        <>
+            <Header />
+            <UserInput onChangeInput={handleChangeInput} userInput={userInput} />
+            {!inputIsValid && <p className="center">Please enter a duration greater than 0.</p>}
+            {inputIsValid && <Results input={userInput} />}
+        </>
+    );
 }
 
-export default App
+export default App;
